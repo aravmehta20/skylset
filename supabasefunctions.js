36a6537f41c8e24cc6db49d80ef54dd3651bc791
aravmehta20 <aravm0720@gmail.com>
2025-04-19 10:38:39 -0500

    Create supabasefunctions.js

---
+++ supabasefunctions.js
@@ -0,0 +1,10 @@
+const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnemZ3aGtqamt4d3NodXB3ZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDM2NDcsImV4cCI6MjA1OTg3OTY0N30.vIMKaY6llWcXPFoKcW7jAnVRcdBOTBACktUN-Mw9Rew"
+const projectURL = "https://tgzfwhkjjkxwshupwdse.supabase.co"
+
+const supabase = createClient(projectURL, anonKey);
+
+
+function editUserData(
+
+
+await supabase.
